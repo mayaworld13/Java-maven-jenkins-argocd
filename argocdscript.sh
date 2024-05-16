074bf7d8a7e1bc9a525c04beaa466f5cc9c2cf64
Mayank sharma <mayank14082003@gmail.com>
2024-05-16 21:33:41 +0530

    Update argocdscript.sh

--- argocdscript.sh
+++ argocdscript.sh
@@ -2,3 +2,9 @@ curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releas
 kubectl create -f https://operatorhub.io/install/argocd-operator.yaml
 kubectl get csv -n operators
 kubectl get csv -n argocd
+kubectl apply -f argocd-basic.yml
+kubectl edit svc example-argocd-server  ## change ClusterIP to NodePort
+minikubeip:example-argocd-server port
+kubectl get secret
+kubectl edit secret example-argocd-cluster ## username is admin and password is given in the file in encoded form with user admin
+echo -n password | base64 -d
